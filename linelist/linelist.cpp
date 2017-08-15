0c52603c0e6c331ff7465d586a279bd8200188a1
reacher <1289581126@qq.com>
2017-08-15 05:11:52 -0700

    test

---
+++ linelist/linelist.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: linelist.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Tue 15 Aug 2017 05:05:07 AM PDT
+ ************************************************************************/
+
+#include<iostream>
+#include "linelist.h"
+using namespace std;
+
+
+
---
+++ linelist/linelist.h
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: linelist.h
+	> Author: 
+	> Mail: 
+	> Created Time: Tue 15 Aug 2017 05:03:31 AM PDT
+ ************************************************************************/
+
+#ifndef _LINELIST_H
+#define _LINELIST_H
+
+#define LIST_LEN 1024
+
+typedef struct tag_list
+{
+    int store[LIST_LEN];
+};
+
+#endif
